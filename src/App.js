78d20a1a84902d58bba42a8b30552f723d1fe308
kpramos-hau <kpramos@student.hau.edu.ph>
2025-06-28 11:15:20 +0800

    Updated App.cs and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const imageUrls = [
+  {url: 'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768', type: 'banana'},
+  {url: 'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg', type:'chicken'},
+  {url: 'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768', type: 'banana'},
+  {url: 'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg', type:'chicken'},
+  {url: 'https://thumbs.dreamstime.com/b/bunch-bananas-6175887.jpg?w=768', type: 'banana'},
+  {url: 'https://thumbs.dreamstime.com/z/full-body-brown-chicken-hen-standing-isolated-white-backgroun-background-use-farm-animals-livestock-theme-49741285.jpg?ct=jpeg', type:'chicken'}
+
+
+];
+
+
+function getRandomImage() {
+  const index = Math.floor(Math.random() * 6) ;
+  return imageUrls[index];
+}
+
 function App() {
+  const [images, setImages] = useState(Array(36).fill().map(() => getRandomImage()));
+  const [playerChoice, setPlayerChoice] = useState(null)
+
+  const handleCellChoice = (image) =>{
+    setImages(images.map(() => getRandomImage()));
+    if (playerChoice === null) return;
+    if (image.type === playerChoice){
+      alert("Correct!")
+    }
+    else{
+      alert("Wrong!" + image.type)
+    }
+  }
+
+  const handleChoice = (choice) => {
+    setPlayerChoice(choice);
+    }
+
+  
+  const handleClick = () => {
+    setImages(images.map(() => getRandomImage()));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Chicken Banana Game!</h1>
+      <div className='button-container'>
+        <button onClick={handleChoice}>Banana</button>
+        <button onClick={handleChoice}>Chicken</button>
+      </div>
+      <div className="grid">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img.url}
+            alt="Random"
+            className="square"
+            onClick={handleCellChoice}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// function Welcome(props){
+//   return <h2>Welcome, {props.name}!</h2>
+// }
+
+// function Counter(){
+//   const[count, setCount] = useState(0)
+
+//   function handleCLick(){
+//     setCount(count + 1)
+//   }
+  
+//   return(
+//     <div>
+//       <p>You clicked {count} times.</p>
+//       <button onClick={handleCLick}>Click Me</button>
+//     </div>
+//   )
+// }
